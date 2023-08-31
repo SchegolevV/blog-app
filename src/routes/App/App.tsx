300ba584a5be3c9aa1c87ab503a0525f4f9cb4d0
Valentin Schegolev <maracas20011@gmail.com>
2023-08-31 16:33:10 +0300

    test

--- src/routes/App/App.tsx
+++ src/routes/App/App.tsx
@@ -1,5 +1,20 @@
 const App = () => {
-  return <div>Hello TS React</div>
+  return (
+    <section>
+      <header>
+        <h1>Hello TS react</h1>
+      </header>
+      <div>
+        <ul>
+          <li>Hard</li>
+          <li>Hard</li>
+          <li>Hard</li>
+          <li>Hard</li>
+          <li>Hard</li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default App
