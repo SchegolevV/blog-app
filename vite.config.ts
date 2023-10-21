afd2bed5874240b4df82300c69a02565b683b6d7
Valentin Schegolev <maracas20011@gmail.com>
2023-10-22 00:49:54 +0300

    font fix test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-// import { resolve } from 'path'
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     '@': resolve('./static/fonts'),
-  //   },
-  // },
 })
